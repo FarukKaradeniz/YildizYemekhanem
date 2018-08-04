7ada1f88ae97ba2769f06f5b2cf9878ba156a10f
Faruk Karadeniz <krdnzomer@gmail.com>
2018-08-04 19:27:03 +0300

    Redesign and bug fix

--- app/src/main/java/com/farukkaradeniz/yildizyemekhanem/SharedPreferencesManager.kt
+++ app/src/main/java/com/farukkaradeniz/yildizyemekhanem/SharedPreferencesManager.kt
@@ -28,19 +28,27 @@ class SharedPreferencesManager(private val pref: SharedPreferences) {
 
     private fun saveOgrenciMenu(day: Int, month: Int, year: Int, menu: YemekMenusu.Ogrenci) {
         with(editor) {
-            putString("O#$day#$month#$year#OM", menu.ogle)
-            putString("O#$day#$month#$year#AM", menu.aksam)
+            putString("O#$day#$month#$year#OM", ifEmpty(menu.ogle, "Ogle Yemek Menusu Bulunamadı"))
+            putString("O#$day#$month#$year#AM", ifEmpty(menu.aksam, "Aksam Yemek Menusu Bulunamadı"))
             apply()
         }
     }
 
     private fun saveAlakartMenu(day: Int, month: Int, year: Int, menu: YemekMenusu.Alakart) {
         with(editor) {
-            putString("A#$day#$month#$year#OM", menu.ogle)
-            putString("A#$day#$month#$year#OA", menu.ogleAlt)
-            putString("A#$day#$month#$year#AM", menu.aksam)
-            putString("A#$day#$month#$year#AA", menu.aksamAlt)
+            putString("A#$day#$month#$year#OM", ifEmpty(menu.ogle, "Ogle Yemek Menusu Bulunamadı"))
+            putString("A#$day#$month#$year#OA", ifEmpty(menu.ogleAlt, "Ogle alternatif Yemek Menusu Bulunamadı"))
+            putString("A#$day#$month#$year#AM", ifEmpty(menu.aksam, "Aksam Yemek Menusu Bulunamadı"))
+            putString("A#$day#$month#$year#AA", ifEmpty(menu.aksamAlt, "Aksam alternatif Yemek Menusu Bulunamadı"))
             apply()
+        }
+    }
+
+    private fun ifEmpty(menu: String, message: String): String {
+        return if (menu.isEmpty()) {
+            message
+        } else {
+            menu
         }
     }
 
